ce5cae65b8d25b0f10695483bd23e831d3369c64
steve <steve.cui2013@gmail.com>
2026-02-10 22:34:12 -0500

    blink

---
+++ variable_blink_v1.ino
@@ -0,0 +1,22 @@
+int duration=5,ledred=6,ledgreen=5,voltage=0;
+void setup() {
+  pinMode(ledred,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  for (voltage=0; voltage<=250;voltage++){
+  analogWrite(ledred,voltage);
+  analogWrite(ledgreen,250-voltage);
+  Serial.println(voltage);
+  delay(duration);
+  }
+  for (voltage=250; voltage>=0;voltage--){
+  analogWrite(ledred,voltage);
+  analogWrite(ledgreen,250-voltage);
+  Serial.println(voltage);
+  delay(duration);
+  }
+  
+}
